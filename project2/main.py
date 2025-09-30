481decf940daad3ec326230852f63e6817d95f39
Ezazul Haq Abdul <61641801+ezazulhaq@users.noreply.github.com>
2025-09-30 13:13:42 +0000

    created a books store object and stored in an array

--- project2/main.py
+++ project2/main.py
@@ -1,3 +1,14 @@
 from fastapi import FastAPI
 
+from project2.model.book import Book
+
 app = FastAPI()
+
+BOOKS = [
+    Book(1, "Title one", "Author one", "Description one", 5),
+    Book(2, "Title two", "Author two", "Description two", 2),
+    Book(3, "Title three", "Author three", "Description three", 1),
+    Book(4, "Title four", "Author one", "Description four", 4),
+    Book(5, "Title five", "Author three", "Description five", 3),
+    Book(6, "Title six", "Author six", "Description six", 4),
+]

7084d3e68198666e99f2750524d15b2e4e9438c8
Ezazul Haq Abdul <61641801+ezazulhaq@users.noreply.github.com>
2025-10-01 11:49:08 +0000

    field level validation added to Book request model

--- project2/dto/book_request.py
+++ project2/dto/book_request.py
@@ -1,8 +1,19 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class BookRequest(BaseModel):
-    id: int
-    title: str
-    author: str
-    description: str
-    rating: int
+    id: int = Field(description="Unique Book ID", default=None)
+    title: str = Field(min_length=2, max_length=15)
+    author: str = Field(min_length=1, max_length=20)
+    description: str = Field(min_length=5, max_length=25)
+    rating: int = Field(gt=0, lt=6)
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "title": "Book Title",
+                "author": "Book Author",
+                "description": "Book Description",
+                "rating": 4
+            }
+        }
+    }
